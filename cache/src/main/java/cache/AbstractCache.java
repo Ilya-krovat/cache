6b50e1c26f9fdac8767a55a3daef07f346d6cc5c
ilya <Mirecch@yandex.ru>
2019-09-11 03:08:21 +0300

    Добавлены тесты

---
+++ cache/src/main/java/cache/AbstractCache.java
@@ -0,0 +1,15 @@
+package cache;
+
+public class AbstractCache implements InterfaceCache
+{
+  @Override
+  public Object get(Object obj)
+  {
+    return null;
+  }
+
+  @Override
+  public void put(Object obj)
+  {
+  }
+}
---
+++ cache/src/main/java/cache/InterfaceCache.java
@@ -0,0 +1,8 @@
+package cache;
+
+public interface InterfaceCache
+{
+  public void put(Object obj);
+
+  public Object get(Object obj);
+}
